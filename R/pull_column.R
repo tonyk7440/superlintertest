9f5e3e6925071f2df8ab859a047d38e9a4f147a2
tonyk7440 <tonyk7440@gmail.com>
2022-10-21 15:45:32 +0100

    try initial commit

---
+++ R/pull_column.R
@@ -0,0 +1,15 @@
+#' Pulls values of select data frame column
+#'
+#' @param data Data frame
+#' @param column_name Column name to pull, in quotes
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' pull_column(iris, "Sepal.Width")
+#'
+pull_column <- function(data, column_name) {
+  data %>%
+    pull(column_name)
+}
